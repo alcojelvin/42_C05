90dfe0a95f0c0b23ea89a26cc186d909d43417b4
Kelvin Neres <kneres.brazil@gmail.com>
2026-04-30 02:17:38 -0300

    updating ex02 main

--- ex02/ft_iterative_power.c
+++ ex02/ft_iterative_power.c
@@ -14,22 +14,18 @@ int ft_iterative_power(int nb, int power)
     return (res);
 }
 
-char    *ft_putnbr(int nb)
+void ft_putnbr(int nb)
 {
     char c;
     if (nb >= 10)
-    {
         ft_putnbr(nb / 10);
-    }
     c = nb % 10 + '0';
     write(1, &c, 1);
 }
 
 int main(void)
 {
-    int nb = 2;
-    int pwr = 4;
-    int res = ft_iterative_power(4, 4);;
-    write(1, ft_putnbr(res), 1);
+    int res = ft_iterative_power(4, 4);
+    ft_putnbr(res);
     write(1, "\n", 1);
 }
